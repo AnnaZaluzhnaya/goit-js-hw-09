0e957ea7cfa575e37f6c4769fe4d9173ed82abde
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-05-16 23:12:20 +0200

    Update 03 Task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formInput = document.querySelector('.form');
+formInput.addEventListener('submit', getNewPromise);
+
+function getNewPromise(event) {
+  event.preventDefault();
+
+  let stepValue = Number(event.currentTarget.step.value);
+  let amountValue = Number(event.currentTarget.amount.value);
+  let delayValue = Number(event.currentTarget.delay.value);
+  
+  for (let position = 1; position <= amountValue; position += 1) {
+    delayValue  += stepValue ;
+    createPromise(position, delayValue)
+      .then(success => Notify.success(`✅ Fulfilled promise ${position} in ${delayValue}ms`))
+      .catch(error => Notify.failure(`❌ Rejected promise ${position} in ${delayValue}ms`));
   }
-}
+};
+  
+  
+function createPromise(position, delayValue) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delayValue });
+        } else {
+          reject({ position, delayValue });
+        }
+      }, delayValue);
+    } )
+};
